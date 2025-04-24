231d37bea3bce9f6a678964e79a2dbfe87bf127d
Michael Stahnke <stahnke@floxdev.com>
2025-04-23 22:46:21 -0500

    Update tests for token expired check

--- test/cantchart.test.js
+++ test/cantchart.test.js
@@ -38,6 +38,63 @@ describe('hubot-cantchart', () => {
       ['hubot', '"**Let\'s take this offline**\r\n\r\n> Critical decisions must be made, but rather than make them and have an effective meeting, somebody just suggests we take an item offline. Then no follow-up ever occurs and thus the only decision that was made was that we\'re not currently making a decision. " -- stahnma'],
     ]));
   });
+
+  context('GitHub token is invalid (401)', () => {
+    beforeEach((done) => {
+      nock('https://api.github.com')
+        .matchHeader('authorization', 'Bearer abcdef')
+        .get('/repos/websages/hates-software/issues/1/comments')
+        .reply(401, {
+          message: 'Bad credentials',
+          documentation_url: 'https://docs.github.com/rest',
+        });
+      room.user.say('alice', 'hubot excuse');
+      setTimeout(done, 100);
+    });
+
+    it('hubot informs user about authentication failure', () => {
+      expect(room.messages).to.eql([
+        ['alice', 'hubot excuse'],
+        ['hubot', 'Authentication with GitHub failed. Please check the `HUBOT_GITHUB_TOKEN`.']
+      ]);
+    });
+  });
+
+  context('GitHub returns empty comment array', () => {
+    beforeEach((done) => {
+      nock('https://api.github.com')
+        .matchHeader('authorization', 'Bearer abcdef')
+        .get('/repos/websages/hates-software/issues/1/comments')
+        .reply(200, []);
+      room.user.say('alice', 'hubot excuse');
+      setTimeout(done, 100);
+    });
+
+    it('hubot informs user there are no excuses', () => {
+      expect(room.messages).to.eql([
+        ['alice', 'hubot excuse'],
+        ['hubot', 'No excuses found today.']
+      ]);
+    });
+  });
+
+  context('GitHub returns malformed JSON', () => {
+    beforeEach((done) => {
+      nock('https://api.github.com')
+        .matchHeader('authorization', 'Bearer abcdef')
+        .get('/repos/websages/hates-software/issues/1/comments')
+        .reply(200, 'not-a-json');
+      room.user.say('alice', 'hubot excuse');
+      setTimeout(done, 100);
+    });
+
+    it('hubot informs user about parsing error', () => {
+      expect(room.messages).to.eql([
+        ['alice', 'hubot excuse'],
+        ['hubot', 'Could not parse response from GitHub.']
+      ]);
+    });
+  });
 });
 
 describe('hubot-cantchart missing config', () => {
@@ -59,10 +116,6 @@ describe('hubot-cantchart missing config', () => {
 
   context('fails with error', () => {
     beforeEach((done) => {
-      nock('https://api.github.com')
-        .matchHeader('authorization', 'Bearer abcdef')
-        .get('/repos/websages/hates-software/issues/1/comments')
-        .replyWithFile(200, './test/fixtures/api_response.json');
       room.user.say('alice', 'hubot excuse');
       setTimeout(done, 100);
     });
@@ -73,3 +126,4 @@ describe('hubot-cantchart missing config', () => {
     ]));
   });
 });
+
